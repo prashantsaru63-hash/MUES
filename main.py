026bd60cd613e936983913c534f5e934f41df3dc
Prashant Saru <prashantsaru63@gmail.com>
2026-04-14 18:15:24 +0545

    Create Menu's

---
+++ main.py
@@ -0,0 +1,26 @@
+
+def menu() :
+
+    print("======== Multi-User Ecommerce System =========")
+    print("1. Rester")
+    print("2. Login")
+    print("3. Exit")
+
+def user_menu() :
+    print(" ===== Customer Menu ======")    
+    print("1. View Product")
+    print("2. Add to cart")
+    print("3. View cart")
+    print("4. Place order")
+    print("5. Logout")
+
+def admin_menu() :
+    print(" ====== Admin Menu =======")    
+    print("1. Add Product")
+    print("2. View All Products")
+    print("3. View orders ")
+    print("4. Logout")
+
+
+
+    
